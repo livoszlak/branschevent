6ef91c5e2f291c7392420f9a9e5d52e81d8ade36
lucasackerberg <lucas@ackerberg.se>
2024-03-20 16:01:07 +0100

    login
    login

---
+++ app/Http/Controllers/LoginController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class LoginController extends Controller
+{
+
+    public function login(Request $request)
+    {
+        $credentials = $request->only('email', 'password');
+
+        if (Auth::attempt($credentials)) {
+            // Authentication passed...
+            return redirect()->intended('/dashboard');
+        }
+
+        return redirect()->route('login')->withErrors(['email' => 'These credentials do not match our records.']);
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,19 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Auth\RegisterController;
+use App\Http\Controllers\Auth\LoginController;
 
+// Basic landing page route
 Route::get('/', function () {
     return view('welcome');
 });
+// Route to register user
+Route::post('/register', [RegisterController::class, 'register'])->name('register');
+// Route to handle the login form submission
+Route::post('/login', [LoginController::class, 'login']);
+// Route to view the dashboard??
+Route::get('/dashboard', function () {
+    return view('dashboard');
+});
+
