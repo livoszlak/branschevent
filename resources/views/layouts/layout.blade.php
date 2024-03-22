726830bc50e0120657c4b5f907cdf2265fe4a531
Liv <110914989+livoszlak@users.noreply.github.com>
2024-03-22 08:22:49 +0100

    Added layout.blade view

---
+++ resources/views/layouts/layout.blade.php
@@ -0,0 +1,18 @@
+<html>
+    <head>
+        <title>@yield('title')</title>
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Inter:wght@100..900&display=swap" rel="stylesheet">
+        <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    </head>
+    {{-- @yield('header') --}}
+    <body>
+        @section('')
+        
+        @show
+ 
+        @yield('content')
+    </body>
+    {{-- @yield('footer') --}}
+</html>
