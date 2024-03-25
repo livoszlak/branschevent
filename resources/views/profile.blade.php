6eefe7c7651ee0cd2d998a5807c4647713a4c4d6
lucasackerberg <lucas@ackerberg.se>
2024-03-25 11:24:28 +0100

    pillbranch
    pillbranch

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -1,7 +1,8 @@
 <section>
     <h3>Hello user</h3>
     <p>here u can change your profile blablabla</p>
-
+  {{--   @dd($user) --}}
+    @dd($profile)
     @if ($editable)
     <!-- Display editable form fields if user is visiting their own profile -->
     <form method="POST" action="{{ route('profile.update', ['profile' => $profile->user_id]) }}">
