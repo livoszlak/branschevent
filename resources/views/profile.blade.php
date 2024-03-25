8d513526d6e45b2006cbf4fc891e737ccaadfcca
Liv <110914989+livoszlak@users.noreply.github.com>
2024-03-25 14:30:08 +0100

    Accidental change on develop while trying to fix profile view

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -2,13 +2,12 @@
     <h3>Hello user</h3>
     <p>here u can change your profile blablabla</p>
 
-    @if ($user->id === $profile->user_id)
+    @if (Auth::user())
     <!-- Display editable form fields if user is visiting their own profile -->
     <form method="POST" action="{{ route('profile.update', ['profile' => $profile->user_id]) }}">
         @csrf
         @method('PUT')
         <!-- form fields, bla bla -->
-        <input type="text">
         <button type="submit">Spara profil</button>
     </form>
     @else
