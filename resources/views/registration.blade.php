7b94ee2b8aaa6809524c74c6f976b30da731bb7c
Liv <110914989+livoszlak@users.noreply.github.com>
2024-03-25 08:00:16 +0100

    Added conditional error message container to register view

--- resources/views/registration.blade.php
+++ resources/views/registration.blade.php
@@ -1,3 +1,13 @@
 <div>
     <!-- Nothing worth having comes easy. - Theodore Roosevelt -->
 </div>
+
+@if ($errors->any())
+    <div class="errors">
+        <ul>
+            @foreach ($errors->all() as $error)
+                <li>{{ $error }}</li>
+            @endforeach
+        </ul>
+    </div>
+@endif
