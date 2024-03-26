26198a1cc311ec350a37aadbb903942a4cba4a99
lucasackerberg <lucas@ackerberg.se>
2024-03-26 16:43:21 +0100

    update profile init commit
    update profile init commit

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -7,10 +7,44 @@
     <form method="POST" action="{{ route('profile.update', ['profile' => $profile->user_id]) }}">
         @csrf
         @method('PUT')
-        <!-- form fields, bla bla -->
-        <input type="text">
-        <button type="submit">Spara profil</button>
+    
+        <!-- Street Name -->
+        <label for="street_name">Street Name:</label>
+        <input type="text" id="street_name" name="street_name" value="{{ old('street_name', $profile->street_name) }}">
+    
+        <!-- Post Code -->
+        <label for="post_code">Post Code:</label>
+        <input type="text" id="post_code" name="post_code" value="{{ old('post_code', $profile->post_code) }}">
+
+        <!-- City -->
+        <label for="city">City:</label>
+        <input type="text" id="city" name="city" value="{{ old('city', $profile->city) }}">
+        
+        <!-- Contact Email -->
+        <label for="contact_email">Contact Email:</label>
+        <input type="email" id="contact_email" name="contact_email" value="{{ old('contact_email', $profile->contact_email) }}">
+        
+        <!-- Contact URL -->
+        <label for="contact_url">Contact URL:</label>
+        <input type="text" id="contact_url" name="contact_url" value="{{ old('contact_url', $profile->contact_url) }}">
+
+        <!-- Contact LinkedIn -->
+        <label for="contact_LinkedIn">Contact LinkedIn:</label>
+        <input type="text" id="contact_LinkedIn" name="contact_LinkedIn" value="{{ old('contact_LinkedIn', $profile->contact_LinkedIn) }}">
+
+        <!-- Has LIA -->
+        <label for="has_LIA">Has LIA:</label>
+        <input type="checkbox" id="has_LIA" name="has_LIA" {{ $profile->has_LIA ? true : false }}>
+        
+        {{-- Tags system later --}}
+
+        <!-- About -->
+        <label for="about">About:</label>
+        <textarea id="about" name="about">{{ old('about', $profile->about) }}</textarea>
+    
+        <button type="submit">Save Profile</button>
     </form>
+    
     @else
     <!-- Display static profile content. Maybe use different layout sections?? -->
     @endif
