1f5648430ab064148c1ce5d5f0d2953e18ae3648
Liv <110914989+livoszlak@users.noreply.github.com>
2024-04-03 13:21:58 +0200

    Commenting out lines to see if that fixes error

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,6 +1,6 @@
-@if(Auth::user()->profile->profile_image)
+{{-- @if(Auth::user()->profile->profile_image)
      <img class="image rounded-circle" src="{{asset('/storage/images/'.Auth::user()->profile->profile_image)}}" alt="profile_image" style="width: 80px;height: 80px; padding: 10px; margin: 0px; ">
-@endif
+@endif --}}
 <!DOCTYPE html>
 <html lang="en">
 <head>
