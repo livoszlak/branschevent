300824f3b7363b679e697d5e3445838bd3366eb4
Liv <110914989+livoszlak@users.noreply.github.com>
2024-04-04 13:16:56 +0200

    Profile picture fix

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,6 +1,3 @@
-{{-- @if(Auth::user()->profile_image)
-     <img class="image rounded-circle" src="{{asset('/storage/images/'.Auth::user()->profile_image)}}" alt="profile_image" style="width: 80px;height: 80px; padding: 10px; margin: 0px; ">
-@endif --}}
 <!DOCTYPE html>
 <html lang="en">
 <head>
