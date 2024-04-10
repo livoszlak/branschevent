e222aac12becbe71fb65874add21c6dfa12e65ec
Liv <110914989+livoszlak@users.noreply.github.com>
2024-04-10 08:59:21 +0200

    Set up blade templates to include only the necessary css & js files on each view. Separated scripts into files named for the views they relate to.

--- resources/views/registration.blade.php
+++ resources/views/registration.blade.php
@@ -1,5 +1,9 @@
 @extends('layouts.app')
 
+@section('css')
+    <link href="{{ asset('css/registration.css') }}" rel="stylesheet">
+@endsection
+
 @section('header')
     {{-- specific header content --}}
     @include('layouts.header')
