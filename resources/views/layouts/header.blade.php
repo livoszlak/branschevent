f5fde899225b3037d3e8bbd21ef5c1b2f7cbde5a
lucasackerberg <lucas@ackerberg.se>
2024-04-12 18:55:35 +0200

    fixes for nav, header and dooter
    fixes for nav, header and dooter

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -7,12 +7,18 @@
         </div>
 
         <div class="blueBlankSpace">
-            <a href="/"><img src="{{asset('pictures/home.png')}}" alt=""></a>
+            <a href="/"><img src="{{asset('pictures/hus.svg')}}" alt=""></a>
         </div>
 
         <div class="headerBranding">
-            <img src="{{asset('pictures/Yrgo.png')}}" alt="">
-            <h1 class="headerh1">YRGO <br> DIGIMIX</h1>
+            <div class="headerBrandinglogo">
+                <img src="{{asset('pictures/yrgotriangle.svg')}}" alt="">
+                <div class="yrgoTextHeader">
+                    <h2 class="removeMargin h3-mobile-bold">YR</h2>
+                    <H2 class="italicHeader removeMargin h3-mobile-bold">GO</H2>
+                </div>
+                <p class="header-p-text">nnect</p>
+            </div>
         </div>
 
     </div>
