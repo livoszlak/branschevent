d7284f0a3a0dec2934a68165168cedf5fc732aa6
lucasackerberg <lucas@ackerberg.se>
2024-04-14 14:52:32 +0200

    fixes
    fixes

--- public/js/welcome.js
+++ public/js/welcome.js
@@ -1,18 +1,25 @@
-/**
- * Button listener
- */
-
 window.addEventListener("scroll", function () {
     var button = document.getElementById("Anmalforetag");
-    var scrollPosition =
-        window.pageYOffset || document.documentElement.scrollTop;
+    var scrollPosition = window.pageYOffset || document.documentElement.scrollTop;
+    var threshold = 0.5;
 
-    var threshold = 2000;
+    // Calculate the position of the stop div
+    var stopDiv = document.getElementById("extraGap");
+    var stopPosition = 1900;
+    console.log(stopPosition);
+    console.log(scrollPosition);
 
     if (scrollPosition > threshold) {
-        button.style.position = "absolute";
-        button.style = "bottom: 200";
-    } else {
         button.style.position = "fixed";
+        button.style.top = 75 + "%";
+
+        // Check if the scroll position is greater than or equal to the stop position
+        if (scrollPosition >= stopPosition) {
+            // If it is, set the button's position to absolute
+            button.style.position = "absolute";
+            button.style.top = stopPosition + 800 + "px";
+        }
+    } else {
+        button.style.position = "absolute";
     }
 });
--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -6,7 +6,7 @@
             <a href="{{ Auth::user() ?  route('profile.show', ['id' => Auth::user()->id]) : url('/login') }}">DIN ANMÄLAN</a>
         </div>
         <div class="footerInfo">
-            <img src="{{ asset('pictures/yrgored.svg') }}" alt="A logo of Yrgo">
+            <img class="footerInfoYrgored" src="{{ asset('pictures/yrgored.svg') }}" alt="A logo of Yrgo">
             <div class="footerInfoColtwo">
                 <ul>
                     <li>Lärdomsgatan 3, <br>
@@ -14,9 +14,9 @@
                     <li>031 - 367 31 00 </li>
                 </ul>
                     <div class="footerLinks">
-                        <a href="https://www.yrgo.se"><img src="{{asset('pictures/websitelink.svg')}}" alt=""></a>
-                        <a href="https://www.facebook.com/yrgogoteborg/"><img src="{{asset('pictures/facebooklogo.svg')}}" alt=""></a>
-                        <a href="https://www.linkedin.com/school/yrgo/"><img src="{{asset('pictures/linkedinlogo.svg')}}" alt=""></a>
+                        <a href="https://www.yrgo.se"><img class="footerLinksimgs" src="{{asset('pictures/websitelink.svg')}}" alt=""></a>
+                        <a href="https://www.facebook.com/yrgogoteborg/"><img class="footerLinksimgs" src="{{asset('pictures/facebooklogo.svg')}}" alt=""></a>
+                        <a href="https://www.linkedin.com/school/yrgo/"><img class="footerLinksimgs" src="{{asset('pictures/linkedinlogo.svg')}}" alt=""></a>
                     </div>
             </div>
         </div>
